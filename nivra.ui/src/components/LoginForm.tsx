a227ca7f948beebad10456f5433bacba19242664
vincingdi <110591050+vincingdi@users.noreply.github.com>
2026-09-21 10:36:07 +0300

    add test api call for user login in

--- nivra.ui/src/components/LoginForm.tsx
+++ nivra.ui/src/components/LoginForm.tsx
@@ -2,14 +2,41 @@ import {useState} from "react";
 
 export default function LoginForm() {
 
+    const  handleSubmit = async (e) => {
 
+        e.preventDefault()
+        /*
+                    const response = await fetch('api/login'), {
+                        method: 'POST',
+                        headers: {
+                            'Content-type': 'application/json',
+                        },
+                        body: JSON.stringify({username, password})
+                    }
+        */
+        
+        try {
+            const response = await fetch('api/DailySteps', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+            })
+
+            if (!response.ok) {
+                throw new Error('Login failed');
+            }
+
+            const data = await response.json()
+            console.log(data)
+        } catch {
+            throw new Error('Login failed');           
+        }
+    }
+    
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-
-    function handleSubmit(e) {
-        console.log(username + " " + password );
-    }
-
+    
     function handleUsernameChange(e) {
 
         setUsername(e.target.value)
@@ -20,16 +47,15 @@ export default function LoginForm() {
 
     }
 
-
     return (
         <div className="header">
             <h1 >Log in</h1>
-            <form onClick={handleSubmit}>
+            <form >
                 <label htmlFor="username">Username: </label>
                 <input type="text" onChange={handleUsernameChange} value={username}></input> <br/><br/>
                 <label htmlFor="password">Password: </label>
                 <input type="password" onChange={handlePasswordChange} value={password}></input> <br/><br/>
-                <input type="submit" value="Submit"/>
+                <input type="submit" value="Submit" onClick={handleSubmit}/>
             </form>
         </div>
     );
