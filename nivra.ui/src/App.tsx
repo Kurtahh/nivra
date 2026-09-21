94868c91771383400c1f424f6c1d115adb635002
vincingdi <110591050+vincingdi@users.noreply.github.com>
2026-09-21 09:19:30 +0300

    add working buttons for signup and login

--- nivra.ui/src/App.tsx
+++ nivra.ui/src/App.tsx
@@ -1,20 +1,23 @@
 import './App.css';
-import LoginForm from './LoginForm.tsx';
-import Home from './Home.tsx';
+import LoginForm from './components/LoginForm.tsx';
+import Home from './components/Home.tsx';
+import SignUpForm from "./components/SignUpForm.tsx";
 
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 export default function App() {
-
+    
   return (
       <BrowserRouter>
           <nav>
               <Link to="/">Home</Link>
               <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
           </nav>
           <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="/login" element={<LoginForm />}> </Route>
+              <Route path="/signup" element={<SignUpForm />}> </Route>
           </Routes>
           
       </BrowserRouter>
---
+++ nivra.ui/src/components/LoginForm.tsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+
+export default function LoginForm() {
+
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSubmit(e) {
+        console.log(username + " " + password );
+    }
+
+    function handleUsernameChange(e) {
+
+        setUsername(e.target.value)
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value)
+
+    }
+
+
+    return (
+        <div className="header">
+            <h1 >Log in</h1>
+            <form onClick={handleSubmit}>
+                <label htmlFor="username">Username: </label>
+                <input type="text" onChange={handleUsernameChange} value={username}></input> <br/><br/>
+                <label htmlFor="password">Password: </label>
+                <input type="password" onChange={handlePasswordChange} value={password}></input> <br/><br/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+
+}
