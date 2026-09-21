5a6ce4e8be5acbb8238a1e2caa5bacbb7fd8f179
vincingdi <110591050+vincingdi@users.noreply.github.com>
2026-09-21 08:21:32 +0300

    add routing between home, login pages

--- nivra.ui/src/App.tsx
+++ nivra.ui/src/App.tsx
@@ -1,33 +1,22 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 import LoginForm from './LoginForm.tsx';
+import Home from './Home.tsx';
 
-type StepRecord = {
-  date: string;
-  stepCount: number;
-  userId: number;
-};
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
 export default function App() {
-  const [records, setRecords] = useState<StepRecord[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:5207/api/DailySteps')
-    .then(res => res.json())
-    .then(data => setRecords(data));
-  }, []);
 
   return (
-      <div className="header">
-        <h1> Nivra</h1>
-        <p>Sveiki!</p>
-        <ul>
-          {records.map((r, i) => (
-            <li key={i}>{r.date} — {r.stepCount} steps (user {r.userId})</li>
-          ))}
-        </ul>
-        <LoginForm /> 
+      <BrowserRouter>
+          <nav>
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+          </nav>
+          <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/login" element={<LoginForm />}> </Route>
+          </Routes>
           
-      </div>
+      </BrowserRouter>
   );
 }
---
+++ nivra.ui/src/Home.tsx
@@ -0,0 +1,28 @@
+import {useEffect, useState} from "react";
+
+type StepRecord = {
+    date: string;
+    stepCount: number;
+    userId: number;
+};
+
+export default function Home() {
+    const [records, setRecords] = useState<StepRecord[]>([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:5207/api/DailySteps')
+            .then(res => res.json())
+            .then(data => setRecords(data));
+    }, []);
+    return (
+        <div className="header">
+            <h1> Nivra</h1>
+            <p>Sveiki!</p>
+            <ul>
+                {records.map((r, i) => (
+                    <li key={i}>{r.date} — {r.stepCount} steps (user {r.userId})</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
